9cef250cfdc08c6ec190f2f87ce72b69c3e80984
russa <aaron.russ@dfki.de>
2019-02-19 15:24:08 +0100

    FIX meta-data

--- config.d.ts
+++ config.d.ts
@@ -1,10 +1,10 @@
 
 /**
- * TODO add/support AMR encoder settings,
+ * TODO add/support FLAC encoder settings,
  * e.g. compression [0,8] (DEFAULT: 5)
  *
  * NOTE would need to be implemented via mmir-plugin-encoder-core(?)
  */
-export interface AMREncoderConfigEntry {
+export interface FlacEncoderConfigEntry {
   flacEncoder?: any;
 }
